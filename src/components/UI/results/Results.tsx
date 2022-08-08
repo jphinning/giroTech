3289e1acf7ce128606f8116d2d35e3a37b0dd407
Joao Hinning <jphinning@gmail.com>
2022-08-08 10:31:23 -0300

    feat:Memoize calculations

--- src/components/UI/results/Results.tsx
+++ src/components/UI/results/Results.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
 import { Wrapper, Title, FlexCollumn, ResultDiv } from "./ResultsStyles";
@@ -19,13 +20,20 @@ function CompoundForm({ children }: CompoundFormProps) {
     (state: RootState) => state.inputs
   );
 
-  const totalAmmount = compoundInterestCalc(
-    investedMoney as unknown as number,
-    deadline as unknown as number,
-    interest as unknown as number
+  const totalAmmount = useMemo(
+    () =>
+      compoundInterestCalc(
+        investedMoney as unknown as number,
+        deadline as unknown as number,
+        interest as unknown as number
+      ),
+    [investedMoney, deadline, interest]
   );
 
-  const totalInterest = totalAmmount - (investedMoney as unknown as number);
+  const totalInterest = useMemo(
+    () => totalAmmount - (investedMoney as unknown as number),
+    [totalAmmount, investedMoney]
+  );
 
   return (
     <Wrapper>
