1dd0475b40f2c0add15e2f1923a076333586da4d
Joao Hinning <jphinning@gmail.com>
2022-08-07 23:48:39 -0300

    feat:form

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import inputValuesReducer from "./slices/inputValuesSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    inputs: inputValuesReducer,
   },
 });
 
